fea8e0fbb11c4c2df73952fcad0e638915bcafc4
Lucy Lima Santos <lucyatmosphere@gmail.com>
2024-11-21 00:56:03 -0300

    feat: added sidebar component and styling to the main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import { Header } from "./components/Header/Header";
+import { Sidebar } from "./components/Sidebar/Sidebar";
+
+import styles from "./App.module.css"
+
+const { wrapper } = styles;
 
 function App() {
   return (
     <>
       <Header />
+      <div className={wrapper}>
+        <Sidebar />
+        <main>
+          post 1
+          post 2
+        </main>
+      </div>
     </>
   );
 }
