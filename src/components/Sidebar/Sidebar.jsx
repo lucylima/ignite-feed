b2ae2133aa787ce8b843552e3aba52bfe41b9153
Lucy Lima Santos <lucyatmosphere@gmail.com>
2024-11-21 10:04:47 -0300

    feat: added profile picture and styling

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,6 +1,7 @@
 import styles from "./Sidebar.module.css";
+import { PencilLine } from "phosphor-react";
 
-const { sidebar, profile, cover } = styles;
+const { sidebar, profile, cover, avatar } = styles;
 export function Sidebar() {
   return (
     <aside className={sidebar}>
@@ -9,11 +10,15 @@ export function Sidebar() {
         src="https://images.unsplash.com/photo-1486707471592-8e7eb7e36f78?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
       />
       <div className={profile}>
+        <img src="https://github.com/lucylima.png" className={avatar} />
         <strong>Lucy Lima</strong>
         <span>Web Developer</span>
       </div>
       <footer>
-        <a href="#">Editar perfil</a>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar perfil
+        </a>
       </footer>
     </aside>
   );
