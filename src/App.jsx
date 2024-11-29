287ef43fa3c6d75a6691e58f54be3d2859855de5
Lucy Lima Santos <lucyatmosphere@gmail.com>
2024-11-29 11:03:31 -0300

    feat: added post component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { Header } from "./components/Header/Header";
 import { Sidebar } from "./components/Sidebar/Sidebar";
+import { Post } from "./components/Post/Post";
 
-import styles from "./App.module.css"
+import styles from "./App.module.css";
 
 const { wrapper } = styles;
 
@@ -12,8 +13,7 @@ function App() {
       <div className={wrapper}>
         <Sidebar />
         <main>
-          post 1
-          post 2
+          <Post />
         </main>
       </div>
     </>
